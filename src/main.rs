026e34a61e037697e0e8834246fad1c12416f2a7
Akihito KIRISAKI <kirisaki@klara.works>
2021-03-22 16:11:57 +0900

    feat: Plant API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+use serde::{Deserialize, Serialize};
+use rocket_contrib::json::Json;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Actcast {
+    count: u64,
+    timestamp: f64,
+    device_name: String,
+}
+
+#[post("/actcast", format = "application/json", data = "<cast>")]
+fn actcast(cast: Json<Actcast>) -> String {
+    cast.count.to_string()
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![actcast, index]).launch();
 }
